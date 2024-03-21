910706942d85f887222aa88c56f332691080b391
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2024-03-21 15:04:46 -0700

    Add files via upload

---
+++ Q7.js
@@ -0,0 +1,18 @@
+"use strict";
+// 7. Number Eight: Write addition , subtraction, multiplication, and division operations that each result in the  nymber 8. Be sure to enclose your operations in print statement to see the results . 
+// Addition
+let a = 10;
+let b = 3;
+console.log(a + b);
+// subtraction
+let c = 10;
+let d = 3;
+console.log(a - b);
+// multiplication
+let e = 10;
+let f = 3;
+console.log(e * f);
+// division
+let g = 10;
+let h = 3;
+console.log(g / f);
---
+++ Q7.ts
@@ -0,0 +1,29 @@
+// 7. Number Eight: Write addition , subtraction, multiplication, and division operations that each result in the  nymber 8. Be sure to enclose your operations in print statement to see the results . 
+
+// Addition
+
+let a: number = 10
+let b: number = 3
+
+console.log(a + b)
+
+// subtraction
+
+let c: number = 10
+let d: number = 3
+
+console.log(a - b)
+
+// multiplication
+
+let e: number = 10
+let f: number = 3
+
+console.log(e * f)
+
+// division
+
+let g: number = 10
+let h: number = 3
+
+console.log(g / f)
